65b421a1d418c271d77e6a6db777c3ac59f66d4c
a hassany <a.hassany@gmail.com>
2011-04-20 22:14:19 -0400

    - Add setup.py for setuptools

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    import distribute_setup
+    distribute_setup.use_setuptools()
+    from setuptools import setup, find_packages
+    
+
+setup(
+    name='PSAPI',
+    version='0.1dev',
+    author='Ahmed El-Hassany',
+    author_email='ahassany@udel.edu',
+    packages=find_packages(exclude=['test']),
+    url='http://code.google.com/p/psapi/',
+    license='GPL',
+    description='Python API for perfSONAR',
+    long_description=open('README').read(),
+)
